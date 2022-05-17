08f807fef6d4c8d68b705fb10b97bacf3951efa8
GitStv <asssigit@gmail.com>
2022-05-17 16:37:23 +0700

    Update ProfilController.php
    rofi

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -14,33 +14,33 @@ class ProfilController extends Controller
     {
 
         $title = "Profile Kami";
-        $identitas = IdentitasSkpd::where('user_id', '3')->where('is_active', 'Active')->first();
-        $profil = Profil::where('user_id', '3')->where('is_active', 'Active')->first();
-        $capaian = Capaianskpd::where('user_id', '3')->where('is_active', 'Active')->first();
+        $identitas = IdentitasSkpd::where('user_id', '10')->where('is_active', 'Active')->first();
+        $profil = Profil::where('user_id', '10')->where('is_active', 'Active')->first();
+        $capaian = Capaianskpd::where('user_id', '10')->where('is_active', 'Active')->first();
         return view('content_frontend.view_profil', compact('title', 'identitas', 'profil', 'capaian'));
     }
 
     public function visimisi()
     {
         $title = "Visi dan Misi Kami";
-        $identitas = IdentitasSkpd::where('user_id', '3')->where('is_active', 'Active')->first();
-        $profil = Profil::where('user_id', '3')->where('is_active', 'Active')->first();
+        $identitas = IdentitasSkpd::where('user_id', '10')->where('is_active', 'Active')->first();
+        $profil = Profil::where('user_id', '10')->where('is_active', 'Active')->first();
         return view('content_frontend.view_visimisi', compact('title', 'identitas', 'profil'));
     }
 
     public function tupoksi()
     {
         $title = "Tugas Pokok dan Fungsi Kami";
-        $identitas = IdentitasSkpd::where('user_id', '3')->where('is_active', 'Active')->first();
-        $profil = Profil::where('user_id', '3')->where('is_active', 'Active')->first();
+        $identitas = IdentitasSkpd::where('user_id', '10')->where('is_active', 'Active')->first();
+        $profil = Profil::where('user_id', '10')->where('is_active', 'Active')->first();
         return view('content_frontend.view_tupoksi', compact('title', 'identitas', 'profil'));
     }
 
     public function struktur()
     {
         $title = "Struktur Organisasi Kami";
-        $identitas = IdentitasSkpd::where('user_id', '3')->where('is_active', 'Active')->first();
-        $profil = Profil::where('user_id', '3')->where('is_active', 'Active')->first();
+        $identitas = IdentitasSkpd::where('user_id', '10')->where('is_active', 'Active')->first();
+        $profil = Profil::where('user_id', '10')->where('is_active', 'Active')->first();
         return view('content_frontend.view_struktur', compact('title', 'identitas', 'profil'));
     }
 }
