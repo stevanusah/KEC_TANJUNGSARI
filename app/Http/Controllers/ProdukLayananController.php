9fec045ad33cbda43cb3493c36a73b53cb639ca0
GitStv <asssigit@gmail.com>
2022-05-17 16:35:30 +0700

    Update ProdukLayananController.php
    rofi

--- app/Http/Controllers/ProdukLayananController.php
+++ app/Http/Controllers/ProdukLayananController.php
@@ -12,17 +12,17 @@ class ProdukLayananController extends Controller
     {
 
         $title = "Produk Layanan Kami";
-        $pl = ProdukLayanan::where('user_id', '3')->where('is_active', 'Active')->first();
-        $pls = DetailLayanan::where('user_id', '3')->where('is_active', 'Active')->orderBy('created_at', 'DESC')->paginate(6);
-        $dpls = DetailLayanan::where('user_id', '3')->where('is_active', 'Active')->get();
+        $pl = ProdukLayanan::where('user_id', '10')->where('is_active', 'Active')->first();
+        $pls = DetailLayanan::where('user_id', '10')->where('is_active', 'Active')->orderBy('created_at', 'DESC')->paginate(6);
+        $dpls = DetailLayanan::where('user_id', '10')->where('is_active', 'Active')->get();
         return view('content_frontend.view_produklayanan', compact('title', 'pl', 'pls', 'dpls'));
     }
 
     public function detail($id)
     {
         $title = "Detail Produk Layanan";
-        $pl = ProdukLayanan::where('user_id', '3')->where('is_active', 'Active')->first();
-        $dtllayanan = DetailLayanan::where('user_id', '3')->where('is_active', 'Active')->where('id', $id)->first();
+        $pl = ProdukLayanan::where('user_id', '10')->where('is_active', 'Active')->first();
+        $dtllayanan = DetailLayanan::where('user_id', '10')->where('is_active', 'Active')->where('id', $id)->first();
         return view('content_frontend.view_detailproduklayanan', compact('title', 'pl', 'dtllayanan'));
     }
 }
